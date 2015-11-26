1a6c753d3ef5c3088a9fe7299f01e9acfac2fb49
unknown <fiore_eliza@hotmail.com>
2015-11-26 12:59:30 +0000

    'v4'

--- models/videos.js
+++ models/videos.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define(
     	'Videos',
       { etiquetaVideo: DataTypes.STRING,
-        direccionVideo: DataTypes.STRING
+        direccionVideo: DataTypes.STRING,
 	etiquetaVideo1: DataTypes.STRING,
         FECHA: DataTypes.STRING
       }
