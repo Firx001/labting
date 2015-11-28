49bec2e9b7eb9c9c0fbfdd407d9c9fce1bdc2676
Firx001 <fiore_eliza@hotmail.com>
2015-11-28 17:37:06 +0100

    version 6.1

--- controllers/videos_controller.js
+++ controllers/videos_controller.js
@@ -6,12 +6,6 @@ exports.index = function(req, res) {
   })
 };
 
-exports.indexrock = function(req, res) {
-  models.Videosrock.findAll().then(function(listado) {
-    res.render('videosrock.ejs', { listado: listado});
-  })
-};
-
 exports.index1 = function(req, res) {
   models.Videos.findAll().then(function(listado) {
     res.render('videos1.ejs', { listado: listado});
@@ -21,5 +15,21 @@ exports.index1 = function(req, res) {
 exports.index2 = function(req, res) {
   models.Videos.findAll().then(function(listado) {
     res.render('videos2.ejs', { listado: listado});
+  })
+};
+
+exports.indexrock = function(req, res) {
+  models.Videosrock.findAll().then(function(listado) {
+    res.render('videosrock.ejs', { listado: listado});
+  })
+};
+exports.indexrock1 = function(req, res) {
+  models.Videosrock.findAll().then(function(listado) {
+    res.render('videosrock1.ejs', { listado: listado});
+  })
+};
+exports.indexrock2 = function(req, res) {
+  models.Videosrock.findAll().then(function(listado) {
+    res.render('videosrock2.ejs', { listado: listado});
   })
 };
