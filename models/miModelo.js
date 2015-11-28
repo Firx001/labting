4caa83d8b5d702308390d18357a082eaf96ff063
Firx001 <fiore_eliza@hotmail.com>
2015-11-28 16:39:42 +0100

    version 6
    probando

--- models/miModelo.js
+++ models/miModelo.js
@@ -17,13 +17,13 @@ var storage  = process.env.DATABASE_STORAGE;
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite con new Sequelize(database, [username=null], [password=null], [options={}])
-// (AquÌ es donde se conecta la BD: video.sqlite)
+// (Aqu√≠ es donde se conecta la BD: video.sqlite)
 /*var sequelize = new Sequelize(null, null, null,
                        {dialect: "sqlite", storage: "video.sqlite"}
                     );
 */
 // Usar BBDD SQLite o Postgres con new Sequelize(database, [username=null], [password=null], [options={}])
-// (AquÌ es donde se conecta la BD: video.sqlite)
+// (Aqu√≠ es donde se conecta la BD: video.sqlite)
 var sequelize = new Sequelize(DB_name, user, pwd,
   { dialect:  protocol,
     protocol: protocol,
@@ -37,9 +37,10 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 // Importar la definicion de la tabla Videos en videos.js
 var Videos = sequelize.import(path.join(__dirname,'videos'));
 
-exports.Videos = Videos; // exportar definiciÛn de tabla Videos
+exports.Videos = Videos; // exportar definici√≥n de tabla Videos
 
-
+var Videosrock = sequelize.import(path.join(__dirname,'videosrock'));
+exports.Videosrock = Videosrock;
 
 // sequelize.sync([options={}]) crea e inicializa tabla de preguntas en DB
 sequelize.sync()
@@ -47,11 +48,27 @@ sequelize.sync()
         // success(..) ejecuta el manejador una vez creada la tabla
         Videos.count()
           .success(function (count){
-             if(count === 0) {   // la tabla se inicializa solo si est· vacÌa
+             if(count === 0) {   // la tabla se inicializa solo si est√° vac√≠a
                 Videos.bulkCreate(
                         [
                           {etiquetaVideo: 'Taylor Swift - Shake It Off',   direccionVideo: 'https://youtu.be/nfWlot6h_JM?list=PL2NnS82s_tdU-oqbHD7PUUduA8MsL8TiD', etiquetaVideo1:' PRUEBAS', FECHA:'2015' },
                           { etiquetaVideo: 'Meghan Trainor - All About That Bass',   direccionVideo: 'https://youtu.be/7PCkvCPvDXk?list=PL2NnS82s_tdU-oqbHD7PUUduA8MsL8TiD',  etiquetaVideo1:' PRUEBAS2', FECHA:'2015' }
+                        ]
+                )
+
+                  .success(function(){console.log('Tabla videos inicializada')});
+              
+              // sequelize.sync([options={}]) crea e inicializa tabla de preguntas en DB
+sequelize.sync()
+  .success(function() {
+        // success(..) ejecuta el manejador una vez creada la tabla
+        Videosrock.count()
+          .success(function (count){
+             if(count === 0) {   // la tabla se inicializa solo si est√° vac√≠a
+                Videosrock.bulkCreate(
+                        [
+                          {etiquetaVideorock: 'Nirvana - Come As You Are',   direccionVideorock: 'https://www.youtube.com/watch?v=vabnZ9-ex7o&list=PLB7LggrPIOMVQkk5W2yqFfuC5uwY07PEV&index=5', etiquetaVideo1rock:' rock alternativo', FECHArock:'1992' },
+                          { etiquetaVideorock: 'R.E.M. - Losing My Religion (Official Music Video)',   direccionVideorock: 'https://www.youtube.com/watch?v=if-UzXIQ5vw',  etiquetaVideo1rock:' rock alternativo', FECHArock:'1991' }
                         ]
                 )
 
